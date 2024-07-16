a87eb23c6f7eca05535aa7ee7c669a65ebe358ab
Andrea Axelsson <120781985+Andrea-Axelsson@users.noreply.github.com>
2024-07-16 10:34:18 +0200

    Update Destinations.tsx

--- src/pages/Destinations.tsx
+++ src/pages/Destinations.tsx
@@ -15,7 +15,55 @@ const Destinations = () => {
         </Link>
       </nav>
 
-      <div className='bg-image-gradient w-auto h-80 rounded-md'></div>
+      <div className='flex bg-image-gradient w-auto h-80 rounded-md'>
+        <div className='flex flex-col items-end justify-end p-6 w-full'>
+          <div className='flex flex-row justify-between w-full'>
+            <article className='flex flex-col'>
+              <h3 className='font-lexend text-14 text-white'>1 Week in</h3>
+              <h1 className='font-lexend text-20 font-bold text-white'>Thailand</h1>
+              <h1 className='font-lexend text-20 text-white'>Phuket</h1>
+            </article>
+            <div className='flex self-end'>
+              <i className="fa-regular fa-heart text-white text-[25px]"></i>
+            </div>
+            
+            {/* <i className="fa-solid fa-heart"></i> */}
+          </div>
+        </div>
+      </div>
+
+      <article>
+        <h2 className='font-lexend text-20 font-bold text-primary-100 mt-6'>
+          Thailand in short
+        </h2>
+        <p className='font-lexend text-14 text-black my-6'>
+        Iceland is a land of dramatic landscapes, from glaciers to volcanoes, where the Northern Lights dance across the night sky, making it a unique travel destination that captivates adventurers.
+        </p>
+      </article>
+      
+      <article>
+        <h2 className='font-lexend text-20 font-bold text-primary-100 mt-6 inline'>
+          Top 5 must visit
+        </h2>
+        <i className="fa-solid fa-chevron-down text-20 text-primary-100 ml-4"></i>
+        <ol style={{ listStyleType: 'decimal' }} className='ml-4'>
+          <li className='font-lexend text-14 text-black my-6'>
+          <span className='font-bold'>Reykjavik:</span> Iceland's capital is a vibrant hub of culture and history. Explore its museums, vibrant streets, and enjoy the lively atmosphere of this charming city.
+          </li>
+          <li className='font-lexend text-14 text-black my-6'>
+          <span className='font-bold'>Reykjavik:</span> Iceland's capital is a vibrant hub of culture and history. Explore its museums, vibrant streets, and enjoy the lively atmosphere of this charming city.
+          </li>
+          <li className='font-lexend text-14 text-black my-6'>
+          <span className='font-bold'>Reykjavik:</span> Iceland's capital is a vibrant hub of culture and history. Explore its museums, vibrant streets, and enjoy the lively atmosphere of this charming city.
+          </li>
+          <li className='font-lexend text-14 text-black my-6'>
+          <span className='font-bold'>Reykjavik:</span> Iceland's capital is a vibrant hub of culture and history. Explore its museums, vibrant streets, and enjoy the lively atmosphere of this charming city.
+          </li>
+          <li className='font-lexend text-14 text-black my-6'>
+          <span className='font-bold'>Reykjavik:</span> Iceland's capital is a vibrant hub of culture and history. Explore its museums, vibrant streets, and enjoy the lively atmosphere of this charming city.
+          </li>
+        </ol>
+      </article>
 
     </section>
     
